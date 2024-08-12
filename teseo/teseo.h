dee233c3f4ccf66ac7ad13c42a688fd8288e4fe7
Jan Cumps <jcumps@gmail.com>
2024-08-12 20:42:22 +0200

    all code independent of stml container type

--- teseo/teseo.h
+++ teseo/teseo.h
@@ -5,7 +5,8 @@
 #include "callbackmanager.h"
 // std::pair
 #include <utility> 
-#include <vector>
+#include <array>
+#include <cassert>
 
 namespace teseo {
 
@@ -27,7 +28,7 @@ class teseo {
 public:
 
     //! constructor.
-    teseo() : single_line_parser(2) {}
+    teseo() : single_line_parser() {}
 
     //! expose the callback manager for writing to Teseo.
     /*!
@@ -89,7 +90,7 @@ public:
 
     //! utility to parse a multiline Teseo reply into separate strings
     /*!
-      \param strings std::vector<qtd::string> reference will get the individual strings.  
+      \param strings auto reference will get the individual strings.  
       \param s constant std::string reference string to be parsed.  
       \param count unsigned int reference gets count of strings parsed.  
       \param command nmea_rr const reference used to validate the status line.  
@@ -97,7 +98,44 @@ public:
 
       split a big Teseo reply in its individual strings. The separator is "\r\n"
     */
-    static bool parse_multiline_reply(std::vector<std::string> & strings, const std::string s, unsigned int& count, const nmea_rr& command) ;
+    static bool parse_multiline_reply(auto& strings, const std::string s, unsigned int& count, const nmea_rr& command) {
+        std::size_t maxelements = strings.size(); // at this moment, don't support growing the array (embedded)
+        std::size_t string_index = 0;
+        std::size_t vector_index; // intentionally uninitialised
+        std::string substring;
+        bool valid = false;
+
+        // TODO: current implementation will reply false if there are more answers than strings.size()
+        // it stores all valid replies up to that point. the remaining ones are discarded.
+        // In the future, I may add a parameter with the max count (default = 0: use strings.size()) 
+        // and rely on the user to provide a container that's big enough for that max count (can assert that)
+    
+        for(vector_index = 0; vector_index < maxelements; vector_index++) {
+            std::size_t new_string_index = s.find("\r\n", string_index);
+            if (new_string_index == std::string::npos) {// exhausted. This should be the status string
+#ifdef __GNUC__ // this requires a recent version of GCC.
+#if __GNUC_PREREQ(10,0)
+                valid = s.substr(string_index, s.length() - string_index).starts_with(command.first.substr(0, command.first.length()-2));
+#else
+                valid = (s.substr(string_index, s.length() - string_index).find((command.first.substr(0, command.first.length()-2)))) != std::string::npos;
+#endif
+#endif
+                break;
+            }
+            assert(vector_index < maxelements);
+            strings[vector_index] = s.substr(string_index, (new_string_index + 2) - string_index); // include the separator
+            valid = strings[vector_index].length() >= 7 && strings[vector_index].substr(3, 4).starts_with(command.second);
+            if (!valid) {
+                vector_index = 0;
+                break;
+            }
+            string_index = new_string_index + 2; // skip the separator
+        }
+        count = vector_index; // report the number of retrieved data lines.
+        std::for_each(strings.begin() + count, strings.end(), [](auto &discard) { 
+            discard = std::string(); });
+        return valid;
+    }
 
     //! write command to the Teseo
     /*!
@@ -130,13 +168,20 @@ public:
     //! send NMEA request to the Teseo and return multi line reply
     /*!
       \param command const nmea_rr reference holds the NMEA command.   
-      \param strings std::vector<std::string> reference gets the replies. 
+      \param strings auto reference gets the replies. 
       \param count unsigned int reference count of strings parsed.  
       \returns  bool true if valid reply 
 
       Send NMEA request that expects more than 1 reply to the Teseo. Validate and Return the repies.
     */    
-    bool ask_nmea_multiple(const nmea_rr& command, std::vector<std::string>& strings, unsigned int& count);
+    bool ask_nmea_multiple(const nmea_rr& command, auto& strings, unsigned int& count) {
+        unsigned int retval; // intentionally not initialised
+        std::string s;
+        write(command.first);
+        read(s);
+        retval = parse_multiline_reply(strings, s, count, command);
+        return retval;
+    }
 
     //! get GLL request to the Teseo and read reply
     /*!
@@ -149,23 +194,28 @@ public:
 
     //! get GSV request to the Teseo and read reply
     /*!
-      \param strings std::vector<std::string> reference gets the reply. 
+      \param strings auto reference gets the reply. 
       \param count unsigned int reference gets count of replies. 
       \returns boold true if validated.
 
       Send request for GSV data to the Teseo. Retrieve the replies.
     */    
-    bool ask_gsv(std::vector<std::string>& strings, unsigned int& count);
+    bool ask_gsv(auto& strings, unsigned int& count) {
+        return ask_nmea_multiple(gsv, strings, count);
+    }
+
 
     //! get GSA request to the Teseo and read reply
     /*!
-      \param strings std::vector<std::string> reference gets the reply. 
+      \param strings auto reference gets the reply. 
       \param count unsigned int reference gets count of replies. 
       \returns boold true if validated.
 
       Send request for GSA data to the Teseo. Retrieve the replies.
     */    
-    bool ask_gsa(std::vector<std::string>& strings, unsigned int& count);
+    bool ask_gsa(auto& strings, unsigned int& count) {
+      return ask_nmea_multiple(gsa, strings, count);
+    }
 
     //! get RMC request to the Teseo and read reply
     /*!
@@ -215,7 +265,7 @@ private:
     //! callback manager for resetting the Teseo
     Callback<void> resetter;
     //! every single line NMEA command has two lines. reply and status
-    std::vector<std::string> single_line_parser;
+    std::array<std::string,2> single_line_parser;
 
 };
 
